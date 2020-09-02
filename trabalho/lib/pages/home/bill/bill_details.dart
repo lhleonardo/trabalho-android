0a19c22d4d3249e0f7d02838113a292c84492593
Leonardo Braz <lhleonardo@hotmail.com>
2020-09-02 17:43:55 -0400

    Melhorias de posicionamento de Card da despesa

--- trabalho/lib/pages/home/bill/bill_details.dart
+++ trabalho/lib/pages/home/bill/bill_details.dart
@@ -3,6 +3,33 @@ import 'package:trabalho/components/member_bill.dart';
 import 'package:trabalho/models/bill.dart';
 
 class BillDetailsPage extends StatelessWidget {
+  String _choiceLabel(String key) {
+    String text = '';
+
+    switch (key) {
+      case 'water':
+        text = 'Água/Esgoto';
+        break;
+      case 'aluguel':
+        text = 'Aluguel e Casa';
+        break;
+      case 'compras':
+        text = 'Compras e Supermercado';
+        break;
+      case 'limpeza':
+        text = 'Limpeza';
+        break;
+      case 'luz':
+        text = 'Luz/Energia elétrica';
+        break;
+      default:
+        text = 'Outros';
+        break;
+    }
+
+    return text;
+  }
+
   @override
   Widget build(BuildContext context) {
     final Bill bill = ModalRoute.of(context).settings.arguments as Bill;
@@ -13,19 +40,33 @@ class BillDetailsPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              Container(
-                margin: const EdgeInsets.only(right: 10),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: IconButton(
-                    icon: const Icon(
-                      Icons.chevron_left,
-                      color: Colors.white,
-                      size: 40,
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.chevron_left,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
                     ),
-                    onPressed: () {},
                   ),
-                ),
+                  Container(
+                    margin: const EdgeInsets.only(right: 20),
+                    child: Text(
+                      'Detalhes da despesa',
+                      style: Theme.of(context).textTheme.headline2,
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(
                 height: 10,
@@ -35,43 +76,63 @@ class BillDetailsPage extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Image(
-                              image: AssetImage(
-                                  'assets/icons/${bill.category}.png'),
-                              height: 80,
-                              width: 80,
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              'Supermercado',
-                              style: Theme.of(context).textTheme.caption,
-                            )
-                          ],
-                        ),
-                        Container(
-                          height: 35,
-                          width: 100,
-                          decoration: BoxDecoration(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(15)),
-                              color: Theme.of(context).accentColor),
-                          child: FlatButton(
-                            child: Text('Pagar',
-                                style: TextStyle(
-                                    color: Theme.of(context).backgroundColor,
-                                    fontSize: 14),
-                                textAlign: TextAlign.center),
-                            onPressed: () {},
+                    const SizedBox(height: 8),
+                    Container(
+                      width: double.infinity,
+                      child: Stack(
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Image(
+                                image: AssetImage(
+                                    'assets/icons/${bill.category}.png'),
+                                height: 80,
+                                width: 80,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    'Categoria',
+                                    style: Theme.of(context).textTheme.caption,
+                                  ),
+                                  Text(
+                                    _choiceLabel(bill.category),
+                                    style: Theme.of(context).textTheme.caption,
+                                  ),
+                                ],
+                              )
+                            ],
                           ),
-                        )
-                      ],
+                          Positioned(
+                            right: 0,
+                            top: 40,
+                            child: Container(
+                              height: 35,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                                color: Theme.of(context).accentColor,
+                              ),
+                              child: FlatButton(
+                                child: Text(
+                                  'Pagar',
+                                  style: TextStyle(
+                                    color: Theme.of(context).backgroundColor,
+                                    fontSize: 14,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                onPressed: () {},
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                     Divider(
                       height: 30,
@@ -157,7 +218,7 @@ class BillDetailsPage extends StatelessWidget {
                       margin: const EdgeInsets.only(top: 10, left: 5, right: 5),
                       child: Column(
                         children: List.generate(
-                          5,
+                          6,
                           (index) => MemberBillWidget(),
                         ),
                       ),
